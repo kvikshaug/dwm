369593766c95d2c90d82e7d3e38635aa7da08288
Ali Kaafarani <ali@kvikshaug.no>
2019-03-17 15:01:36 +0100

    Add mplayer floating rule

--- config.h
+++ config.h
@@ -28,6 +28,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "MPlayer",  NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
